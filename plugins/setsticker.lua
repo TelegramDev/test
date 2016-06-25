ac6ec170c4e1def85136ec34411660acd8b613ab
TelegramDev <makan.j110110@gmail.com>
2016-06-25 14:03:39 +0430

    Update setsticker.lua

--- plugins/setsticker.lua
+++ plugins/setsticker.lua
@@ -1,12 +1,13 @@
 local function tosticker(msg, success, result)
   local receiver = get_receiver(msg)
   if success then
-    local file = '/vps/test/data/moon.webp'
+    local file = 'sticker.webp'
     print('File downloaded to:', result)
     os.rename(result, file)
     print('File moved to:', file)
     send_document(get_receiver(msg), file, ok_cb, false)
-    redis:del("photo:sticker")
+	send_large_msg(receiver, 'Done!', ok_cb, false)
+    redis:del("photo:setsticker")
   else
     print('Error downloading: '..msg.id)
     send_large_msg(receiver, 'Failed, please try again!', ok_cb, false)
@@ -16,20 +17,25 @@ local function run(msg,matches)
     local receiver = get_receiver(msg)
     local group = msg.to.id
     if msg.media then
-       if msg.media.type == 'photo' and redis:get("photo:sticker") then
-        if redis:get("photo:sticker") == 'waiting' then
+       if msg.media.type == 'photo' and redis:get("photo:setsticker") then
+        if redis:get("photo:setsticker") == 'waiting' then
           load_photo(msg.id, tosticker, msg)
         end
        end
     end
-    if matches[1] == "setsticker" and is_sudo(msg) then
-     redis:set("photo:sticker", "waiting")
+    if matches[1]:lower() == "setsticker" and is_sudo(msg) then
+     redis:set("photo:setsticker", "waiting")
      return 'Please send your photo now'
     end
+	if matches[1]:lower() == 'moon' then --[[Your bot name]]
+	send_document(get_receiver(msg), "sticker.webp", ok_cb, false)
+end
 end
 return {
   patterns = {
- "^[!#/](setsticker)$",
+ "^(setsticker)$",
+ "^([Mm][Oo][Oo][Nn])$",
+ "^[!#/]([Mm]oon)$",
  "%[(photo)%]",
   },
   run = run,
